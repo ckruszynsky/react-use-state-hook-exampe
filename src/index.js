f82f3d693df606986dc0220762677db6c25ce84b
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-12-30 12:10:40 -0600

    initial project setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
+  const [age, setAge] = useState(21);
+  const handleAgeUp = () => {
+    setAge(age + 1);
+  };
+
+  const handleAgeDown = () => {
+    setAge(age - 1);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <h1>Use State Hook</h1>
+        <h2>Age: {age}</h2>
+        <button onClick={handleAgeUp}>Age up</button>
+        <button onClick={handleAgeDown}>Age down</button>
+      </header>
     </div>
   );
 }
